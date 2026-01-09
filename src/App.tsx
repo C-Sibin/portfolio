6a5f49532d1391a0998b969825b25554a0845129
Xyper-X <shadowcyber2004@gmail.com>
2026-01-09 13:06:34 +0530

    New Commit

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ const App = () => (
       <TooltipProvider>
         <Toaster />
         <Sonner />
-        <HashRouter basename="/portfolio">
+        <HashRouter>
           <Routes>
             <Route path="/" element={<Index />} />
             <Route path="/blog" element={<Blog />} />
